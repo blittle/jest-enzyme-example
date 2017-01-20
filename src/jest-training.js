daa096c85a0d17f813a133070eafcc7a1cfb988a
Bret Little <bret.little@gmail.com>
2017-01-20 11:26:01 -0700

    Working!!

--- src/jest-training.js
+++ src/jest-training.js
@@ -3,6 +3,8 @@ import styles from './jest-training.styles.css';
 
 export default class MyComponent extends React.Component {
 	render() {
-		return <div onClick={this.props.clicked} className={styles.wrapper}>Hello It changed</div>
+		return <div onClick={this.props.clicked} className={styles.wrapper}>
+			Hello It changed
+		</div>
 	}
 }
--- src/jest-training.test.js
+++ src/jest-training.test.js
@@ -4,4 +4,22 @@ import renderer from 'react-test-renderer';
 import {shallow} from 'enzyme';
 
 describe('jest training', function() {
+	it('should click', function() {
+		const spy = jasmine.createSpy();
+
+		const wrapper = shallow(<MyComponent clicked={spy} />);
+		wrapper.find('div').simulate('click');
+
+		expect(spy).toHaveBeenCalled();
+
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it('should render', function() {
+		const component = renderer.create(
+			<MyComponent />
+		);
+		let tree = component.toJSON();
+		expect(tree).toMatchSnapshot();
+	});
 });
