53d2b08315580f655836cb034af6d532d651cbb3
Bret Little <bret.little@gmail.com>
2017-01-19 17:30:47 -0700

    Add stuff

--- src/jest-training.js
+++ src/jest-training.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import styles from './jest-training.styles.css';
 
-export default class MyComponent extends React {
+export default class MyComponent extends React.Component {
 	render() {
-		return <div className={styles.wrapper}>Hello</div>
+		return <div onClick={this.props.clicked} className={styles.wrapper}>Hello It changed</div>
 	}
 }
