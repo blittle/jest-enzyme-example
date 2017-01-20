ad10638176ed3867970f3a970b2157bcd1195e61
Bret Little <bret.little@gmail.com>
2017-01-20 09:16:04 -0700

    Update things

--- src/jest-training.test.js
+++ src/jest-training.test.js
@@ -4,20 +4,4 @@ import renderer from 'react-test-renderer';
 import {shallow} from 'enzyme';
 
 describe('jest training', function() {
-	// it('should render', function() {
-	// 	const component = renderer.create(
-	// 		<MyComponent />
-	// 	);
-	// 	let tree = component.toJSON();
-	// 	expect(tree).toMatchSnapshot();
-	// });
-  //
-	// it('should click', function() {
-	// 	const spy = jasmine.createSpy();
-  //
-	// 	const wrapper = shallow(<MyComponent clicked={spy} />);
-	// 	wrapper.find('div').simulate('click');
-  //
-	// 	expect(spy).toHaveBeenCalled();
-	// });
 });
